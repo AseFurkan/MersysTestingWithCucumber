98f6e7ed66a7ca31f761bba0e115504cd16828fc
AseFurkan <furkan1asefler@gmail.com>
2023-10-14 14:48:28 +0300

    Login

---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,36 @@
+package StepDefinitions;
+
+import Utilities.ExcelUtility;
+import Utilities.GWD;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+
+    @After
+    public void after(Scenario senaryo)
+    {
+        ExcelUtility.writeToExcel("src/test/java/ApachePOI/resource/SenaryoSonuclari3.xlsx",
+                senaryo);
+
+        if (senaryo.isFailed()){
+            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
+            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+        }
+        GWD.quitDriver();
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,32 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Utilities.GWD;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_LoginSteps {
+    DialogContent dc=new DialogContent();
+
+    @Given("Navigate to Campus")
+    public void navigate_to_campus() {
+        GWD.getDriver().get("https://test.mersys.io/");
+
+    }
+
+    @When("Enter username and password and click login button")
+    public void enter_username_and_password_and_click_login_button() {
+        dc.mySendKeys(dc.username, "turkeyts");
+        dc.mySendKeys(dc.password, "TechnoStudy123");
+        dc.myClick(dc.loginButton);
+    }
+
+    @Then("User should login successfully")
+    public void user_should_login_successfully() {
+
+         dc.verifyContainsText(dc.txtTechnoStudy,"Techno Study");
+
+    }
+
+}
